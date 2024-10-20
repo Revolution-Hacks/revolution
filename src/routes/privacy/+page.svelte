a36e070c2aded5e7911bd896c2529cc8cd807342
Sarah <144068104+iamawatermelo@users.noreply.github.com>
2024-10-20 17:34:37 +0100

    add a description to the page

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -3,7 +3,8 @@
 
   let glossary = {
     'data subject': 'someone whose information we have collected',
-    'data processing agreement': 'the agreement between us and a third party that states their rights and obligations to us and to you concerning the protection of personal data',
+    'data processing agreement':
+      'the agreement between us and a third party that states their rights and obligations to us and to you concerning the protection of personal data',
     'personal data': 'any data that could be used to identify someone, for example, your name or email address',
     // "sensitive personal data":
     // "personal data that requires more protection because it is sensitive, like your racial or ethnic origin or information about your health",
@@ -17,6 +18,13 @@
     'encrypted in flight': 'data that is encrypted while it is being transferred from one machine to another'
   };
 </script>
+
+<svelte:head>
+  <meta
+    name="description"
+    content="This privacy policy outlines how we collect, store and process your data, as well as your rights under the UK Data Protection Act 2018."
+  />
+</svelte:head>
 
 {#snippet term(definition)}
   <a href={`#term-${definition.replaceAll(' ', '')}`} title={glossary[definition]}>{definition}</a>
@@ -65,8 +73,8 @@
 
   {@render header('information-collected', 'What information do we collect and why?')}
   <p>
-    In general, we do not collect information that you do not explicitly provide to us. However, when you visit our
-    site or interact with our emails, we may collect:
+    In general, we do not collect information that you do not explicitly provide to us. However, when you visit our site
+    or interact with our emails, we may collect:
   </p>
   <ul>
     <li>your {@render term('IP address')},</li>
@@ -80,11 +88,11 @@
     interested would help us budget for providing travel grants to people.
   </p>
   <p>When you sign up to get emails from us, we store your email so that we can send you an email later.</p>
-  
-  {@render header('storage', "How do you store my data?")}
+
+  {@render header('storage', 'How do you store my data?')}
   <p>
-    We store and process your data with various third-party services.
-    Here are some of our third-party services and their {@render term('data processing agreement')}s:
+    We store and process your data with various third-party services. Here are some of our third-party services and
+    their {@render term('data processing agreement')}s:
   </p>
   <ul>
     <li><a href="https://postmarkapp.com/dpa">Postmark</a></li>
@@ -93,10 +101,10 @@
   </ul>
   <p>
     Your data is {@render term('encrypted at rest')} and {@render term('encrypted in flight')} using secure encryption algorithms.
-    We take your personal information seriously, and our team members only have access to the data they need
-    to carry out their work. 
+    We take your personal information seriously, and our team members only have access to the data they need to carry out
+    their work.
   </p>
-  
+
   {@render header('retention', 'When will you delete my information?')}
   <p>
     We will securely delete your information soon after the event, and no later than one year after you have provided it
