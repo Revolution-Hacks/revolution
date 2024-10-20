4cf38f759fe8cbf61ef3d96ee2e7ec60fc1248b1
Sarah <144068104+iamawatermelo@users.noreply.github.com>
2024-10-20 23:53:15 +0100

    Revert "updates to the wording of the privacy policy"
    This reverts commit 05a767e0c7de443eda8d0b5f7b06770f60e0e98b.

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -2,23 +2,20 @@
   import Page from '$lib/components/Page.svelte';
 
   let glossary = {
-    'Data Subject': 'The person we have collected data from.',
-    'Data Processing Agreement':
-      'The agreement between us and a third party that states their rights and obligations to us and to them concerning the protection of their personal data.',
-    'Personal Data':
-      'Any data that could be used to identify someone, for example, names, email address or IP addresses.',
-    'Sensitive Personal Data':
-      'Any data that, under the DPA, must be treated with extra care, such as infomation relating to Race/Ethnicity, Health, Political Opinions and more.',
-    'IP address': 'A series of numbers that represents the network you are currently on.',
-    'Approximate Location':
-      'An estimated location based on your IP address, for example, the country and city you live in.',
-    'Legitimate Interests':
-      'Processing your data for the interests of Revolution or third parties while balancing your interests, rights and freedoms.',
-    'User Agent':
-      'A piece of text that identifies the browser or email client you are using, as well as what device and operating system you use.',
-    'Encrypted at Rest': 'Data that is encrypted while it is being stored and not used.',
-    'Encrypted in Flight / Transmission':
-      'Data that is encrypted while it is being transferred from one machine to another.'
+    'data subject': 'someone whose information we have collected',
+    'data processing agreement':
+      'the agreement between us and a third party that states their rights and obligations to us and to you concerning the protection of personal data',
+    'personal data': 'any data that could be used to identify someone, for example, your name or email address',
+    // "sensitive personal data":
+    // "personal data that requires more protection because it is sensitive, like your racial or ethnic origin or information about your health",
+    'IP address': 'a series of numbers that represents the network you are currently on',
+    'approximate location': 'an estimated location based on your IP address, for example, the country you live in',
+    'legitimate interests':
+      'processing your data for the interests of Revolution or third parties while balancing your interests, rights and freedoms',
+    'user agent':
+      'a piece of text that identifies the browser or email client you are using, as well as what device and operating system you use',
+    'encrypted at rest': 'data that is encrypted while it is being stored and not used',
+    'encrypted in flight': 'data that is encrypted while it is being transferred from one machine to another'
   };
 </script>
 
@@ -82,24 +79,20 @@
   <ul>
     <li>your {@render term('IP address')},</li>
     <li>
-      your {@render term('Approximate Location')} (for example, the country you live in, but not your home address),
+      your {@render term('approximate location')} (for example, the country you live in, but not your home address),
     </li>
-    <li>your {@render term('User Agent')}.</li>
+    <li>your {@render term('user agent')}.</li>
   </ul>
   <p>
-    We collect this information to improve Revolution, which allows us to improve the event experience for you. For
-    example, if we know how many people from outside the UK are interested in attending Revolution, we can approximate
-    how much funding we will need to set aside for travel grants.
+    We collect this information to improve Revolution. For example, knowing that many people from other countries are
+    interested would help us budget for providing travel grants to people.
   </p>
-  <p>
-    When you sign up to recieve emails from us, we store your email so that we can send you emails relating to
-    Revolution in the future.
-  </p>
+  <p>When you sign up to get emails from us, we store your email so that we can send you an email later.</p>
 
   {@render header('storage', 'How do you store my data?')}
   <p>
     We store and process your data with various third-party services. Here are some of our third-party services and
-    their {@render term('Data Processing Agreement')}s:
+    their {@render term('data processing agreement')}s:
   </p>
   <ul>
     <li><a href="https://postmarkapp.com/dpa">Postmark</a></li>
@@ -107,18 +100,16 @@
     <li><a href="https://cloud.google.com/terms/data-processing-addendum/?hl=en">Google Workspace</a></li>
   </ul>
   <p>
-    Your data is {@render term('Encrypted at Rest')} and {@render term('Encrypted in Flight')} using secure encryption algorithms.
-    We consider the protection of your personal information to be our highest priority. Our teams only have access to the
-    data nessecary to fufil their roles, and nothing more.
+    Your data is {@render term('encrypted at rest')} and {@render term('encrypted in flight')} using secure encryption algorithms.
+    We take your personal information seriously, and our team members only have access to the data they need to carry out
+    their work.
   </p>
 
   {@render header('retention', 'When will you delete my information?')}
   <p>
-    We will securely erase all of your personal data with the exception of name and email address no later than 1 month
-    after the conclusion of Revolution, and we will securely erase your name and email address no later than one year
-    after you have provided it to us. Under the Data Protection Act, you have the right to erasure. If you would like
-    your information deleted sooner, you may reach out to us at
-    <a href="mailto:policy@revohacks.com">policy@revohacks.com</a>.
+    We will securely delete your information soon after the event, and no later than one year after you have provided it
+    to us. If you would like your information deleted sooner, you may reach out to us at
+    <a href="mailto:hello@revohacks.com">hello@revohacks.com</a>.
   </p>
 
   {@render header('rights', 'What are my rights as a data subject?')}
@@ -131,31 +122,26 @@
     </li>
     <li>
       <b>The right to access</b><br />
-      You have the right to know what information we hold about you in a human readable format at any time upon request.
+      You have the right to know what informatin we hold about you.
     </li>
     <li>
       <b>The right to rectification</b><br />
-      We must update our records of you if you tell us that they are wrong or inaccurate.
+      We must update our records of you if you tell us that they are wrong.
     </li>
     <li>
       <b>The right to restrict processing</b><br />
-      You have the right to ask us to limit the ways in which we process your {@render term('Personal Data')}.
+      You have the right to ask us to limit the ways in which we process your {@render term('personal data')}.
     </li>
     <li>
       <b>The right to data portability</b><br />
-      You have the right to obtain and reuse your {@render term('Personal Data')} across different services.
+      You have the right to obtain and reuse your {@render term('personal data')} across different services.
     </li>
     <li>
       <b>The right to object</b><br />
-      You may object to our processing of your {@render term('Personal Data')} that is collected for
-      {@render term('Legitimate Interests')}.
+      You may object to our processing of your {@render term('personal data')} that is collected for
+      {@render term('legitimate interests')}.
     </li>
   </ul>
-
-  <p>
-    To make subject access requests, update your records, or exercise any other rights you hold under the Data
-    Protection Act, please reach out to us via <a href="mailto:policy@revohacks.com">policy@revohacks.com</a>
-  </p>
   <p>
     <strong>This is not legal advice.</strong> Learn more on
     <a href="https://ico.org.uk/for-organisations/uk-gdpr-guidance-and-resources/individual-rights/individual-rights/">
@@ -165,7 +151,7 @@
 
   {@render header('contact', 'Who do I contact regarding my personal data?')}
   <p>
-    Please contact <a href="mailto:policy@revohacks.com">policy@revohacks.com</a>.
+    Please contact <a href="mailto:hello@revohacks.com">hello@revohacks.com</a>.
   </p>
 </Page>
 
