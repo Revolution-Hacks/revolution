8e4df923695eda823adb0c67215a2726cf430fa9
Sarah <144068104+iamawatermelo@users.noreply.github.com>
2024-10-20 17:32:41 +0100

    add information about data storage and processing

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -3,6 +3,7 @@
 
   let glossary = {
     'data subject': 'someone whose information we have collected',
+    'data processing agreement': 'the agreement between us and a third party that states their rights and obligations to us and to you concerning the protection of personal data',
     'personal data': 'any data that could be used to identify someone, for example, your name or email address',
     // "sensitive personal data":
     // "personal data that requires more protection because it is sensitive, like your racial or ethnic origin or information about your health",
@@ -11,7 +12,9 @@
     'legitimate interests':
       'processing your data for the interests of Revolution or third parties while balancing your interests, rights and freedoms',
     'user agent':
-      'a piece of text that identifies the browser or email client you are using, as well as what device and operating system you use'
+      'a piece of text that identifies the browser or email client you are using, as well as what device and operating system you use',
+    'encrypted at rest': 'data that is encrypted while it is being stored and not used',
+    'encrypted in flight': 'data that is encrypted while it is being transferred from one machine to another'
   };
 </script>
 
@@ -34,6 +37,7 @@
   <ul>
     <li><a href="#key-terms">Key terms</a></li>
     <li><a href="#information-collected">What information do we collect and why?</a></li>
+    <li><a href="#storage">How do you store my data?</a></li>
     <li><a href="#retention">When will you delete my information?</a></li>
     <li><a href="#rights">What are my rights as a data subject?</a></li>
     <li><a href="#contact">Who do I contact regarding my personal data?</a></li>
@@ -76,7 +80,23 @@
     interested would help us budget for providing travel grants to people.
   </p>
   <p>When you sign up to get emails from us, we store your email so that we can send you an email later.</p>
-
+  
+  {@render header('storage', "How do you store my data?")}
+  <p>
+    We store and process your data with various third-party services.
+    Here are some of our third-party services and their {@render term('data processing agreement')}s:
+  </p>
+  <ul>
+    <li><a href="https://postmarkapp.com/dpa">Postmark</a></li>
+    <li><a href="https://www.airtable.com/company/dpa">Airtable</a></li>
+    <li><a href="https://cloud.google.com/terms/data-processing-addendum/?hl=en">Google Workspace</a></li>
+  </ul>
+  <p>
+    Your data is {@render term('encrypted at rest')} and {@render term('encrypted in flight')} using secure encryption algorithms.
+    We take your personal information seriously, and our team members only have access to the data they need
+    to carry out their work. 
+  </p>
+  
   {@render header('retention', 'When will you delete my information?')}
   <p>
     We will securely delete your information soon after the event, and no later than one year after you have provided it
