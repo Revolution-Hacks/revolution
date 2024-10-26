5963605217bdaa9d7f6167bfa5934722982333a8
Sarah <144068104+iamawatermelo@users.noreply.github.com>
2024-10-26 17:25:35 +0100

    :3

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,23 @@
 import TitleFontURL from '$lib/fonts/DMSans.title.woff2?url';
 import TextFontURL from '$lib/fonts/DMSans.text.woff2?url';
+import Bomb from '$lib/bomb.zstd?arraybuffer'
 import type { Handle } from '@sveltejs/kit';
 
 const PRELOADED_ASSETS = [TitleFontURL, TextFontURL];
 
 export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event);
+  
+  // To be silly, send 2 gigabytes of <div>
+  if (response.status === 404 && event.url.pathname.includes("wp")) {
+    return new Response(Bomb, {
+      status: 200,
+      headers: {
+        'Content-Encoding': 'zstd',
+        'Content-Type': 'text/html'
+      }
+    })
+  }
 
   // Disable JS preloading. This took hours to find.
   // I hate web development.
