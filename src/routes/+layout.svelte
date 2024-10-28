5436205fab3e1b823fba10960e9ccb9132ca6d3c
Sarah <144068104+iamawatermelo@users.noreply.github.com>
2024-10-28 00:07:27 +0000

    stop downloading the same font twice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,7 +50,7 @@
   @font-face {
     font-family: 'Revolution Sans ASCII';
     font-display: block;
-    src: url('$lib/fonts/DMSans.text.woff2?url'), local('DM Sans');
+    src: url('$lib/fonts/DMSans.text.woff2'), local('DM Sans');
   }
 
   // Global font styles
