6ba7171abdba1a6b59513133774aef547b478edc
Sarah <144068104+iamawatermelo@users.noreply.github.com>
2024-10-28 13:16:40 +0000

    use JavaScript to auto-submit the form + migrate Page to Svelte 5

--- src/routes/email/subscribe/+page.svelte
+++ src/routes/email/subscribe/+page.svelte
@@ -1,26 +1,37 @@
 <script>
   import Page from '$lib/components/Page.svelte';
-  import ErrorPage from '../../+error.svelte';
-  import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+  
+  let noscriptDiv = $state();
+  let form = $state();
+  
+  onMount(() => {
+    form.submit();
+  })
 </script>
 
-{#if $page.status === 200}
-  <Page title="Confirm your email address">
+<Page title="Confirm your email address">
+  <noscript>
     <h1>Just one more step</h1>
     <p>Just so we know it's you trying to subscribe, please click the button below to proceed.</p>
-    <form method="POST">
-      <button type="submit">Verify my email address</button>
-    </form>
-    <details>
-      <summary>Why do I have to do this?</summary>
-      Sometimes, software like an antivirus opens all links before you do to check that they are safe. We can't tell whether
-      it's you that has clicked the link in the email or a robot. This is why you have to press the button to make sure it's
-      you trying to subscribe.
-    </details>
-  </Page>
-{:else}
-  <ErrorPage />
-{/if}
+    <!-- This must be @html, otherwise, it will be put into the CSS bundle which we don't want -->
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html `<style> .noscript { display: none } </style>`}
+  </noscript>
+  <div class="noscript" bind:this={noscriptDiv}>
+    <h1>Please wait</h1>
+    <p>We're confirming your e-mail address. If nothing happens within a few seconds, you can click the button below.</p>
+  </div>
+  <form method="POST" bind:this={form}>
+    <button type="submit">Verify my email address</button>
+  </form>
+  <details>
+    <summary>Why do I have to do this?</summary>
+    Sometimes, software like an antivirus opens all links before you do to check that they are safe. We can't tell whether
+    it's you that has clicked the link in the email or a robot. This is why you have to press the button to make sure it's
+    you trying to subscribe.
+  </details>
+</Page>
 
 <style lang="scss">
   @use 'src/lib/style';
